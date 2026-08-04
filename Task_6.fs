7b694292d220aa2947b3b1d7c5a0f8ae878c2f31
navydragon <navydragon@inbox.ru>
2026-08-04 11:15:06 +0300

    fix test 6

--- Task_6.fs
+++ Task_6.fs
@@ -6,10 +6,13 @@ let rec pow = function
 | (s, n) -> s + pow (s, n - 1)
 
 // 17.2
-let isIthChar (s: string, n, c) = s.[n] = c
+let isIthChar  = function
+| (s: string, n, _) when n < 0 || n >= String.length s -> false
+| (s, n, c) -> s.[n] = c
 
 // 17.3
 let rec occFromIth = function
+| (s, n, c) when n < 0 -> occFromIth (s, 0, c)
 | (s: string, n, _) when n >= String.length s -> 0
 | (s, n, c) when s.[n] = c -> 1 + occFromIth (s, n + 1, c)
 | (s, n, c) -> occFromIth (s, n + 1, c)
--- Task_6_tests.fsx
+++ Task_6_tests.fsx
@@ -7,6 +7,12 @@ let assertEqual name expected actual =
     else
         printfn "FAIL %s: expected %A, got %A" name expected actual
 
+let assertNoException name expected f =
+    try
+        assertEqual name expected (f ())
+    with ex ->
+        printfn "FAIL %s: unexpected exception %s" name ex.Message
+
 printfn "--- pow ---"
 assertEqual "abc x 3"  "abcabcabc" (pow ("abc", 3))
 assertEqual "x x 1"    "x"         (pow ("x", 1))
@@ -18,6 +24,9 @@ assertEqual "s.[2] = '3'" true  (isIthChar ("12345", 2, '3'))
 assertEqual "s.[0] = '1'" true  (isIthChar ("12345", 0, '1'))
 assertEqual "s.[1] != 'a'" false (isIthChar ("12345", 1, 'a'))
 assertEqual "s.[4] = '5'" true  (isIthChar ("12345", 4, '5'))
+assertNoException "s.[-1] не падает" false (fun () -> isIthChar ("12345", -1, '1'))
+assertNoException "s.[5] не падает" false (fun () -> isIthChar ("12345", 5, '1'))
+assertNoException "empty.[0] не падает" false (fun () -> isIthChar ("", 0, 'a'))
 
 printfn "--- occFromIth ---"
 assertEqual "a с 0" 4 (occFromIth ("abacada", 0, 'a'))
@@ -27,3 +36,6 @@ assertEqual "a с 6" 1 (occFromIth ("abacada", 6, 'a'))
 assertEqual "a с 7" 0 (occFromIth ("abacada", 7, 'a'))
 assertEqual "x в abc" 0 (occFromIth ("abc", 0, 'x'))
 assertEqual "b с 0" 1 (occFromIth ("abc", 0, 'b'))
+assertNoException "a с -2 не падает" 4 (fun () -> occFromIth ("abacada", -2, 'a'))
+assertNoException "a с 100 не падает" 0 (fun () -> occFromIth ("abacada", 100, 'a'))
+assertNoException "empty с 0 не падает" 0 (fun () -> occFromIth ("", 0, 'a'))
